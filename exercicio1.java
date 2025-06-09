51d8fdf62dc527e94dddb7ce410ec9bacaeaf7b2
Guilherme Silva Costa <gui31488@gmail.com>
2025-06-09 19:42:57 -0300

    atividade 1 do dia 2204

---
+++ exercicio1.java
@@ -0,0 +1,15 @@
+package exercicio01;
+
+
+public class exercicio1 {
+
+    public static void main(String[] args) {
+        
+        
+        //Mensagem de bem vindo
+        
+        System.out.println("Bem Vindo(a) ao Java!");
+          
+        
+    }
+}
